ecad42e0cadb9f04aa2b2f3db90bf61d836cc685
iGotjah <niek.werkman@gmail.com>
2019-05-10 13:47:32 +0200

    first commit

---
+++ XmlCrud/App_Code/SongMethods.cs
@@ -0,0 +1,36 @@
+﻿using System.Data; 
+using System;
+using System.Collections.Generic;
+using System.Web;
+public class SongMethods
+{
+
+    public DataSet GetAllSongs(string file)
+    {
+        DataSet ds = new DataSet("playlist");
+
+        DataTable dtSongs = new DataTable("song");
+
+        DataColumn dcId = new DataColumn("id");
+        DataColumn dcArtist = new DataColumn("artist");
+        DataColumn dcTitle = new DataColumn("title");
+        DataColumn dcYear = new DataColumn("year");
+        DataColumn dcGenre = new DataColumn("genre");
+        DataColumn dcTime = new DataColumn("time");
+        DataColumn dcFile = new DataColumn("file");
+
+        dtSongs.Columns.Add(dcId);
+        dtSongs.Columns.Add(dcArtist);
+        dtSongs.Columns.Add(dcTitle);
+        dtSongs.Columns.Add(dcYear);
+        dtSongs.Columns.Add(dcGenre);
+        dtSongs.Columns.Add(dcTime);
+        dtSongs.Columns.Add(dcFile);
+        ds.Tables.Add(dtSongs);
+
+        ds.ReadXml(HttpContext.Current.Server.MapPath(file));
+
+        return ds;
+    }
+
+}
